a5d43a8bdcb300a278b14fe01e40ac22edab2f7a
ilya-1001 <ikobtsev83@yandex.ru>
2025-05-10 22:52:25 +0300

    Add files via upload

---
+++ flask/client.py
@@ -0,0 +1,28 @@
+import requests
+
+
+# response = requests.post(
+#     "http://127.0.0.1:5000//api/v1/ads",
+#     json={"title": "ad_5", "description": "qwerty", "owner": "user"},
+# )
+# print(response.status_code)
+# print(response.json())
+
+# response = requests.delete(
+#     "http://127.0.0.1:5000/api/v1/ads/1",
+# )
+# print(response.status_code)
+# print(response.json())
+
+# response = requests.get(
+#     "http://127.0.0.1:5000//api/v1/ads/4",
+# )
+# print(response.status_code)
+# print(response.json())
+
+# response = requests.patch(
+#     "http://127.0.0.1:5000//api/v1/ads/2",
+#     json={"title": "buy cur", "description": "no new", "owner": "user_15"},
+# )
+# print(response.status_code)
+# print(response.json())
